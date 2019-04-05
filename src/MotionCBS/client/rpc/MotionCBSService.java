f08f4a007edbae32020425e40e43d9ee892b2042
Sebzj <49274502+Sebzj@users.noreply.github.com>
2019-04-05 10:47:04 +0200

    Rettet bogstav rettelse i RPC

--- src/MotionCBS/client/rpc/MotionCBSService.java
+++ src/MotionCBS/client/rpc/MotionCBSService.java
@@ -3,7 +3,7 @@ package MotionCBS.client.rpc;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-import com.motionCBS.client.MotionCBS;
+import MotionCBS.client.MotionCBS;
 
 @RemoteServiceRelativePath("MotionCBSService")
 public interface MotionCBSService extends RemoteService {
--- src/MotionCBS/client/rpc/MotionCBSServiceAsync.java
+++ src/MotionCBS/client/rpc/MotionCBSServiceAsync.java
@@ -2,7 +2,7 @@ package MotionCBS.client.rpc;
 
 import MotionCBS.client.MotionCBS;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.motionCBS.client.MotionCBS;
+import MotionCBS.client.MotionCBS;
 
 public interface MotionCBSServiceAsync {
     void getMessage(String msg, AsyncCallback<String> async);
