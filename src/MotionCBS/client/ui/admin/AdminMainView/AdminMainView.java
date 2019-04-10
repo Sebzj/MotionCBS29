c6db88605f98e4d4ad7e39517c2c2948a7ff5096
Sebzj <49274502+Sebzj@users.noreply.github.com>
2019-04-10 13:01:44 +0200

    Lavet AdminCreateUserview samt AdminMainView

--- src/MotionCBS/client/ui/admin/AdminMainView/AdminMainView.java
+++ src/MotionCBS/client/ui/admin/AdminMainView/AdminMainView.java
@@ -2,12 +2,20 @@ package MotionCBS.client.ui.admin.AdminMainView;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DeckLayoutPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
 public class AdminMainView extends Composite {
     interface AdminMainViewUiBinder extends UiBinder<HTMLPanel, AdminMainView> {
     }
+
+    @UiField
+    Button logoutBtn;
+    @UiField
+    DeckLayoutPanel centerPanel;
 
     private static AdminMainViewUiBinder ourUiBinder = GWT.create(AdminMainViewUiBinder.class);
 
